b95db363118dcc4b3bac90626da4e707a92f15c0
Sebastian Eklund Larsen <SDU\selar16@imada-106320.adm.c.sdu.dk>
2019-12-22 04:41:57 +0100

    yes

---
+++ assignment2/Option1_Atom-Atom/test.py
@@ -0,0 +1,27 @@
+import pydoStuff
+from pydoStuff import *
+
+# sanity check for multiple copies of libMØD
+modValue = mod.magicLibraryValue()
+ourValue = pydoStuff.magicLibraryValue()
+if modValue != ourValue:
+	print("mod =", modValue)
+	print("our =", ourValue)
+	raise Exception("Magic values differ! I.e., more than one instance of libmod has been loaded.")
+# end of sanity check
+
+# make doStuff a bit more friendly to use
+_doStuff_orig = pydoStuff.doStuff
+def _doStuff(educts, products):
+	return mod._unwrap(_doStuff_orig(
+		mod._wrap(mod.VecGraph, educts),
+		mod._wrap(mod.VecGraph, products)))
+pydoStuff.doStuff = _doStuff
+doStuff = _doStuff
+# end of friendlyfier code
+
+g1 = smiles("OCC=O")
+g2 = smiles("OC=CO")
+res = doStuff([g1], [g2])
+for a in res:
+	a.print()
