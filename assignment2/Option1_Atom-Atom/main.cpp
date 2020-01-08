52ba59cd26442a387824c974d81628b4b5b045db
lazh01 <selar16@student.sdu.dk>
2020-01-08 04:54:07 +0100

    asasas

--- assignment2/Option1_Atom-Atom/main.cpp
+++ assignment2/Option1_Atom-Atom/main.cpp
@@ -162,7 +162,7 @@ void Permutate(AutoTypes gEduct, AutoTypes gProduct, std::map<int, int> *EtoP, s
 						std::cout << pMolProduct[j] << "\t\"" << pMolEduct[v] << "\"" << std::endl;
 						EtoP->insert(std::pair<int, int>(getVertexId(v, gEduct), getVertexId(j, gProduct)));
 						PtoE->insert(std::pair<int, int>(getVertexId(j, gProduct), getVertexId(v, gEduct)));
-						Permutate(gEduct, gProduct, EtoP, PtoE, pMolEduct, pMolProduct));
+						Permutate(gEduct, gProduct, EtoP, PtoE, pMolEduct, pMolProduct);
 						EtoP->erase(getVertexId(v, gEduct));
 						PtoE->erase(getVertexId(j, gProduct));
 					}
@@ -237,7 +237,7 @@ std::vector<std::shared_ptr<mod::rule::Rule> > doStuff(const std::vector<std::sh
 	
 	std::map<int, int> EtoP;
 	std::map<int, int> PtoE;
-	Permutate(gEduct, gProduct, &EtoP, &PtoE, pMolEduct, pMolProduct);
+	Permutate(gEduct, gProduct, EtoP, PtoE, pMolEduct, pMolProduct);
 	
 
 	
