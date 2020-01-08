b11d194f2fc0071ec4435eaeb2545223330ebd88
lazh01 <selar16@student.sdu.dk>
2020-01-08 12:28:30 +0100

    asasa

--- assignment2/Option1_Atom-Atom/main.cpp
+++ assignment2/Option1_Atom-Atom/main.cpp
@@ -93,6 +93,7 @@ template <typename ChemGraph>
 std::set<int> *verify(ChemGraph gEduct, ChemGraph gProduct, std::map<int, int> *EtoP, std::map<int, int> *PtoE, molList pMolEduct, molList pMolProduct, VertexMap *vertexMap, std::vector<VertexMap> *vertexMaps)
 {
 	std::set<int> cycle;
+	std::set<int> ret;
 	std::map<int, int> Oedges;
 	std::map<int, int> Xedges;
 	/*std::map<int, int>::iterator it;
@@ -125,7 +126,7 @@ std::set<int> *verify(ChemGraph gEduct, ChemGraph gProduct, std::map<int, int> *
 					{
 						if (bondValue(pMolEduct[e]) > bondValue(pMolProduct[ep]))
 						{
-							return std::set<int> *ret;
+							return &ret;
 						}
 					}
 				}
@@ -165,7 +166,7 @@ std::set<int> *verify(ChemGraph gEduct, ChemGraph gProduct, std::map<int, int> *
 					{
 						if (bondValue(pMolProduct[e]) > bondValue(pMolEduct[ep]))
 						{
-							return std::set<int> *ret;
+							return &ret;
 						}
 					}
 				}
@@ -190,7 +191,7 @@ std::set<int> *verify(ChemGraph gEduct, ChemGraph gProduct, std::map<int, int> *
 
 	if (Oedges.size() != Xedges.size())
 	{
-		return;
+		return &ret;
 	}
 
 	int first = Oedges.begin()->first;
@@ -208,7 +209,7 @@ std::set<int> *verify(ChemGraph gEduct, ChemGraph gProduct, std::map<int, int> *
 		{
 			if (Xedges.find(current) == Xedges.end())
 			{
-				return std::set<int> *ret;
+				return &ret;
 			}
 			tmp = Xedges.find(current)->second;
 			Xedges.erase(current);
@@ -219,7 +220,7 @@ std::set<int> *verify(ChemGraph gEduct, ChemGraph gProduct, std::map<int, int> *
 		{
 			if (Oedges.find(current) == Oedges.end())
 			{
-				return std::set<int> *ret;
+				return &ret;
 			}
 			tmp = Oedges.find(current)->second;
 			Oedges.erase(current);
@@ -236,7 +237,7 @@ std::set<int> *verify(ChemGraph gEduct, ChemGraph gProduct, std::map<int, int> *
 		validMap(&cycle, EtoP, gEduct, gProduct, vertexMap, vertexMaps);
 		return &cycle;
 	}
-	return std::set<int> *ret;
+	return &ret;
 }
 
 template <typename AutoTypes>
@@ -245,6 +246,7 @@ void Permutate(AutoTypes gEduct, AutoTypes gProduct, std::map<int, int> *EtoP, s
 {
 		//Finds unmapped vertex from educt and maps it to an unmapped vertex from product with the same atom symbol
 		std::set<int> *check;
+		std::set<int> empty;
 		std::set<int>::iterator it;
 		for (const auto v : asRange(vertices(gEduct)))
 		{
@@ -263,7 +265,7 @@ void Permutate(AutoTypes gEduct, AutoTypes gProduct, std::map<int, int> *EtoP, s
 						if((i == check->size()) && (check->size() > 0)){
 							return;
 						} else if (check->size() > i){
-							check = std::set<int> b;
+							check = &empty;
 						}
 						//std::cout << pMolProduct[j] << "\t\"" << pMolEduct[v] << "\"" << std::endl;
 						EtoP->insert(std::pair<int, int>(getVertexId(v, gEduct), getVertexId(j, gProduct)));
